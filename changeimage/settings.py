a7f331c3f9587ba9108ae188607f533aa61a919a
pivarsha <varsha7022001@gmail.com>
2023-08-04 00:47:57 +0530

    second

--- changeimage/settings.py
+++ changeimage/settings.py
@@ -113,7 +113,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -121,6 +121,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR/"static"
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- imgaeapiapp/views.py
+++ imgaeapiapp/views.py
@@ -4,12 +4,14 @@ from .serializers import *
 from rest_framework.views import APIView
 import random
 from rest_framework.response import Response
+from django.conf import settings
 
 class ImageApi(APIView):
     def get(self,request):
         images = Image.objects.all()
         if images:
             image = random.choice(images)
+            # image_url = f"{settings.MEDIA_URL}{image.img}"
             image_url = image.img.url
         else:
             image_url = "IMG-20230801-WA0006.jpg"
